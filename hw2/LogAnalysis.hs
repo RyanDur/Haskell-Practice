f6f25f6645dcb1be1540d558e3c136bd17ee918d
RyanDur <ryan.p.durling@gmail.com>
2014-12-22 00:44:03 +0000

    hm2 ex1

--- hw2/LogAnalysis.hs
+++ hw2/LogAnalysis.hs
@@ -1,10 +1,27 @@
 {-# OPTIONS_GHC -Wall #-}
 module LogAnlysis where
-
+import Data.List
 import Log
 
+-- ex1
 parseMessage :: String -> LogMessage
-parseMessage ('E':xs) = LogMessage Error
-parseMessage ('I':xs) = Unknown xs
-parseMessage ('W':xs) = Unknown xs
-parseMessage _ = Unknown _
+parseMessage ('E':xs) =
+  let err = Error((read(head(words xs))))
+      tStamp = read(head(tail(words xs))) :: TimeStamp
+      message = intercalate " " (tail(tail(words xs)))
+  in LogMessage err tStamp message
+
+parseMessage ('I':xs) =
+  let tStamp = read(head(words xs)) :: TimeStamp
+      message = intercalate " " (tail(words xs))
+  in LogMessage Info tStamp message
+
+parseMessage ('W':xs) =
+  let tStamp = read(head(words xs)) :: TimeStamp
+      message = intercalate " " (tail(words xs))
+  in LogMessage Warning tStamp message
+
+parseMessage xs = Unknown xs
+
+parse :: String -> [LogMessage]
+parse text = [parseMessage line | line <- lines text]
